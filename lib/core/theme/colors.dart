421f69c4d2ed386cac28bfe6e771d3405a5dce43
ismaelnascimento <ismael.nascimento08@aluno.ifce.edu.br>
2025-09-25 15:30:31 -0300

    Configuração do projeto

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const colorPrimary = Color.fromARGB(255, 13, 126, 231);
+const colorSecond = Color(0xFFE3EDF6);
+
+const colorBackground = Color(0x0ff00000);
---
+++ lib/core/theme/sizes.dart
@@ -0,0 +1,4 @@
+const sizePadding = 16.0;
+const sizeSpacing = 16.0;
+const sizeAppBar = 48.0;
+const sizeRadius = 16.0;
