138eef0c12451be320f3c815a588e890ac667265
ismaelnascimento <ismael.nascimento08@aluno.ifce.edu.br>
2025-10-01 17:40:26 -0300

    Adicionando form

--- lib/views/auth/login_view_mael.dart
+++ lib/views/auth/login_view_mael.dart
@@ -7,6 +7,8 @@ class LoginViewMael extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+
     return Scaffold(
       extendBody: true,
       backgroundColor: Colors.black,
@@ -20,14 +22,14 @@ class LoginViewMael extends StatelessWidget {
                 gradient: LinearGradient(
                   colors: [
                     Colors.black,
-                    Colors.transparent, 
                     Colors.transparent,
-                    Colors.black
+                    Colors.transparent,
+                    Colors.black,
                   ],
                   begin: Alignment.topCenter,
                   end: Alignment.bottomCenter,
                   stops: const [0.05, 0.4, 0.7, 1],
-                )
+                ),
               ),
               child: Image.network(
                 "https://cdn.polyspeak.ai/speakmaster/4747b3658f61e5da7f14fddc670a15df.webp",
@@ -52,13 +54,38 @@ class LoginViewMael extends StatelessWidget {
                   width: double.maxFinite,
                   child: Container(
                     alignment: Alignment.topCenter,
-                    padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 24),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 32,
+                      vertical: 24,
+                    ),
                     child: Column(
+                      spacing: 16,
                       children: [
-                        Text("Liquid Glass Sample", style: ThemeData.dark().textTheme.displayLarge,),
-                        FlutterLogo(size: 100,),
+                        Form(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              Text(
+                                "Para cadastrar-se, preencha os campos abaixo:",
+                                style: AppTextStyles.bodyLargeBright,
+                              ),
+                              Text("Input"),
+                              Row(
+                                children: [
+                                  Text(
+                                    "Mostrar senha",
+                                    style: AppTextStyles.bodyMedium.copyWith(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  // Checkbox(value: showPassword, onChanged: (value) => togglePasswordVisibility(value)),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
                       ],
-                    ) ,
+                    ),
                   ),
                 ),
               ),
@@ -69,3 +96,62 @@ class LoginViewMael extends StatelessWidget {
     );
   }
 }
+
+
+/* 
+LIQUID GLASS EFFECT:
+
+ Container(
+              alignment: Alignment.bottomCenter,
+              child: LiquidGlass(
+                settings: LiquidGlassSettings(
+                  blur: 4,
+                  glassColor: Colors.white.withAlpha(25),
+                  ambientStrength: 0.5,
+                  lightIntensity: 1,
+                ),
+                shape: LiquidRoundedRectangle(
+                  borderRadius: Radius.circular(16),
+                ),
+                child: SizedBox(
+                  height: 500,
+                  width: double.maxFinite,
+                  child: Container(
+                    alignment: Alignment.topCenter,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 32,
+                      vertical: 24,
+                    ),
+                    child: Column(
+                      spacing: 16,
+                      children: [
+                        Form(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              Text(
+                                "Para cadastrar-se, preencha os campos abaixo:",
+                                style: AppTextStyles.bodyLargeBright,
+                              ),
+                              Text("Input"),
+                              Row(
+                                children: [
+                                  Text(
+                                    "Mostrar senha",
+                                    style: AppTextStyles.bodyMedium.copyWith(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  // Checkbox(value: showPassword, onChanged: (value) => togglePasswordVisibility(value)),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+*/
