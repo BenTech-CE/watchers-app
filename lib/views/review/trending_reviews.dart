8c8a2d18fe8f8f628b6d841373612d5f02225008
João Gabriel Aguiar de Senna <gabriel.joao61@aluno.ifce.edu.br>
2025-12-03 17:00:19 -0300

    Adicionando telas de Reviews e Lists Populares.

---
+++ lib/views/review/trending_reviews.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:watchers/core/providers/reviews/reviews_provider.dart';
+import 'package:watchers/core/theme/texts.dart';
+import 'package:watchers/widgets/review_card.dart';
+
+class TrendingReviews extends StatefulWidget {
+  const TrendingReviews({super.key});
+
+  @override
+  State<TrendingReviews> createState() => _TrendingReviewsState();
+}
+
+class _TrendingReviewsState extends State<TrendingReviews> {
+  @override
+  Widget build(BuildContext context) {
+    final reviewProvider = context.watch<ReviewsProvider>();
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        title: Text("Resenhas populares", style: AppTextStyles.bodyLarge.copyWith(fontSize: 22, fontWeight: FontWeight.w600),),
+      ),
+      body: ListView.separated(
+        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+        separatorBuilder: (BuildContext context, int index) {
+          return const SizedBox(height: 23,);
+        },
+        itemCount: reviewProvider.trendingReviews.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ReviewCard(review: reviewProvider.trendingReviews[index]);
+        },
+      ),
+    );
+  }
+}
